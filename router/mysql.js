b33eb09243e3eb2535ebb22f329abddeb02f6eed
zmf <1094178787@qq.com>
2020-10-26 20:57:12 +0800

    换用连接池解决连接失效问题

--- router/mysql.js
+++ router/mysql.js
@@ -1,28 +1,35 @@
 var mysql = require('mysql');
-var connection = null;
+var pool = null;
 
-//连接以及重连
-function handleError(err) {
-    if (err) {
-        // 如果是连接断开，自动重新连接
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            connect();
-        } else {
-            console.error(err.stack || err);
-        }
-    }
-}
+// //连接以及重连
+// function handleError(err) {
+//     if (err) {
+//         // 如果是连接断开，自动重新连接
+//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//             connect();
+//         } else {
+//             console.error(err.stack || err);
+//         }
+//     }
+// }
 
-function connect() {
-    connection = mysql.createConnection({
-        host: "47.101.146.224",
-        user: 'taiji',
-        password: '',
-        database: 'taiji_web'
-    });
-    connection.connect(handleError);
-    connection.on('error', handleError);
-}
-connect();
+// function connect() {
+//     connection = mysql.createConnection({
+//         host: "47.101.146.224",
+//         user: 'taiji',
+//         password: '',
+//         database: 'taiji_web'
+//     });
+//     connection.connect(handleError);
+//     connection.on('error', handleError);
+// }
+// connect();
 
-module.exports=connection;
+
+pool = mysql.createPool({
+    host: "47.101.146.224",
+    user: 'taiji',
+    password: '',
+    database: 'taiji_web'
+});
+module.exports = pool;
